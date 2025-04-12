1afb104aa5572491903a2fe2a1624f40dca5c6e9
Michael <michael>
2025-04-12 12:01:17 +0300

    lesson5

--- 05-dom-events/main.js
+++ 05-dom-events/main.js
@@ -1,14 +1,28 @@
 const cards = [
-    {name:'', img:''}
-    {name:'', img:''}
-    {name:'', img:''}
-    {name:'', img:''}
-    {name:'', img:''}
-    {name:'', img:''}
-    {name:'', img:''}
-    {name:'', img:''}
-    {name:'', img:''}
-    {name:'', img:''}
-    {name:'', img:''}
-    {name:'', img:''}
+    {name:'bullbasaur', img:'bullbasaur.png'},
+    {name:'charmander', img:'charmander.png'},
+    {name:'eevee', img:'eevee.png'},
+    {name:'pikachu', img:'pikachu.png'},
+    {name:'psyduck', img:'psyduck.png'},
+    {name:'squirtle', img:'squirtle.png'},
+    
+    {name:'bullbasaur', img:'bullbasaur.png'},
+    {name:'charmander', img:'charmander.png'},
+    {name:'eevee', img:'eevee.png'},
+    {name:'pikachu', img:'pikachu.png'},
+    {name:'psyduck', img:'psyduck.png'},
+    {name:'squirtle', img:'squirtle.png'},
 ]
+
+let madolWindowHelp = document.getElementsByClassName('modal-help')[0]
+console.log('modal-help=', madolWindowHelp)
+
+let button = madolWindowHelp.getElementsByTagName('button')
+console.log('button=', button)
+
+for(let i = 0; i < button.length; i++){
+    button[i].onclick = function(e) {
+        madolWindowHelp.style.display = 'none'
+    }
+
+}
